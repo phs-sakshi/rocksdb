33a08240cd21aeba1cbc9f734c261f53c8a382e8
anweshasaha <anwesha.saha284@gmail.com>
2023-08-28 20:29:47 -0400

    added namespace

--- examples/primary_rocksdb_sample.cc
+++ examples/primary_rocksdb_sample.cc
@@ -8,24 +8,25 @@
 #include <chrono>
 #include "clipp.h"
 
+using namespace std;
 
-std::string generateRandomKey(int keySize) {
-  std::string key;
+string generateRandomKey(int keySize) {
+  string key;
   for (int i = 0; i < keySize; i++) {
     int digit = rand() % 10;
-    key += std::to_string(digit);
+    key += to_string(digit);
   }
   return key;
 }
 
 
-std::string generateRandomValue(size_t length) {
+string generateRandomValue(size_t length) {
     static constexpr char chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     static constexpr size_t charsCount = sizeof(chars) - 1;
-    static std::mt19937 gen(std::random_device{}());
-    std::uniform_int_distribution<size_t> dist(0, charsCount - 1);
+    static mt19937 gen(random_device{}());
+    uniform_int_distribution<size_t> dist(0, charsCount - 1);
 
-    std::string result;
+    string result;
     result.reserve(length);
     for (size_t i = 0; i < length; ++i) {
         result += chars[dist(gen)];
@@ -41,7 +42,7 @@ bool coinToss() {
 
 int main(int argc, char* argv[]) {
 
-  std::string database_path= "./testdb";
+  string database_path= "./testdb";
   int num_reads = 200;
   int num_entries = 1000;
   int num_writes = 300;
@@ -57,64 +58,84 @@ int main(int argc, char* argv[]) {
   );
 
   if (!parse(argc, argv, cli)) {
-    std::cout << make_man_page(cli, argv[0]) << std::endl;
+    cout << make_man_page(cli, argv[0]) << endl;
     return 1;
   }
   rocksdb::DB* db;
   rocksdb::Options options;
   options.create_if_missing = true;
-  // rocksdb::Status status = rocksdb::DB::Open(options, "./testdb", &db);
-  rocksdb::Status status = rocksdb::DB::Open(options, database_path, &db);
-  assert(status.ok());
+  // options.error_if_exists = true; // Set error_if_exists option to true
 
-  std::unordered_set<std::string> randomKeys;
-  std::string value = "value";
+  rocksdb::Status status = rocksdb::DB::Open(options, database_path, &db);
+  if (status.ok()) {
+    cout << "Database opened successfully!" << endl;
+  } else {
+    cerr << "Failed to open database: " << status.ToString() << endl;
+  }
+  options.disable_auto_compactions = false;
+  options.use_direct_reads = false; // or false
+  options.IncreaseParallelism();
+
+
+  // rocksdb::Status status = rocksdb::DB::Open(options, database_path, &db);
+  // assert(status.ok());
+
+  unordered_set<string> randomKeys;
+  string value = "value";
 
   rocksdb::WriteBatch batch;
 
   for (int i = 0; i < num_entries; i++) {
-    std::string key = generateRandomKey(keySize);
-    std::string value = generateRandomValue(keySize);
+    string key = generateRandomKey(keySize);
+    string value = generateRandomValue(keySize);
     batch.Put(key, value);
   }
 
   status = db->Write(rocksdb::WriteOptions(), &batch);
   assert(status.ok());
 
-  auto startWrite = std::chrono::steady_clock::now();
+  auto startWrite = chrono::steady_clock::now();
 
   for (int i = 0; i < num_writes; i++) {
-    std::string key = generateRandomKey(keySize);
-    std::string value = generateRandomValue(keySize);
+    string key = generateRandomKey(keySize);
+    string value = generateRandomValue(keySize);
     bool tossResult = coinToss();
     if (tossResult == 0 && randomKeys.size() < num_reads) {
-       .insert(key);
+       randomKeys.insert(key);
     }
-    std::cout << "Generated key: " << key << std::endl;
+    // cout << "Generated key: " << key << endl;
     status = db->Put(rocksdb::WriteOptions(), key, value);
     assert(status.ok());
   }
 
-  auto endWrite = std::chrono::steady_clock::now();
+  auto endWrite = chrono::steady_clock::now();
 
-  std::cout << "randomKeys size= " << randomKeys.size() << std::endl;
+  // cout << "randomKeys size= " << randomKeys.size() << endl;
 
-  std::string read_value;
+  string read_value;
 
-  auto startRead = std::chrono::steady_clock::now();
-  for (const std::string& key : randomKeys) {
+  auto startRead = chrono::steady_clock::now();
+  for (const string& key : randomKeys) {
     status = db->Get(rocksdb::ReadOptions(), key, &read_value);
     assert(status.ok());
-    std::cout << "Read val " << key << ": " << read_value << std::endl;
+    // cout << "Read val " << key << ": " << read_value << endl;
   }
-  auto endRead = std::chrono::steady_clock::now(); 
+  auto endRead = chrono::steady_clock::now(); 
 
-  delete db;
+  // delete db;
 
-  auto writeDuration = std::chrono::duration_cast<std::chrono::milliseconds>(endWrite - startWrite);
-  auto readDuration = std::chrono::duration_cast<std::chrono::milliseconds>(endRead - startRead);
+  uint64_t totalBytesWritten, totalBytesRead;
+  db->GetIntProperty("rocksdb.bytes.written", &totalBytesWritten);
+  db->GetIntProperty("rocksdb.bytes.read", &totalBytesRead);
 
-  std::cout << "Write Time: " << writeDuration.count() << " ms" << std::endl;
-  std::cout << "Read Time: " << readDuration.count();
+
+  cout << "Total Bytes Written: " << totalBytesWritten << endl;
+  cout << "Total Bytes Read: " << totalBytesRead << endl;
+
+  auto writeDuration = chrono::duration_cast<chrono::milliseconds>(endWrite - startWrite);
+  auto readDuration = chrono::duration_cast<chrono::milliseconds>(endRead - startRead);
+
+  cout << "Write Time: " << writeDuration.count() << " ms" << endl;
+  cout << "Read Time: " << readDuration.count();
   return 0;
 }
